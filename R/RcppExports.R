436de6531bbe3fe39ac22bcf5fa69cd7375b29a8
mpadge <mark.padgham@email.com>
2023-03-14 13:33:40 +0100

    add initial src files from pboettch/json-schema and nlohmann/json

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -4,6 +4,7 @@
 #' src_fn
 #'
 #' A test C++ function
+#' @srrstats {G2.3} in src directory
 #' @noRd
 test <- function() {
     .Call(`_jsonschema_test`)
--- src/cpptest.cpp
+++ src/cpptest.cpp
@@ -3,6 +3,7 @@
 //' src_fn
 //'
 //' A test C++ function
+//' @srrstats {G2.3} in src directory
 //' @noRd
 // [[Rcpp::export]]
 int test () {
